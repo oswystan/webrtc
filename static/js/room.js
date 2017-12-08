a05d66df4f5537d699d8a730c2fc5a2b867b0fd3
oswystan <oswystan@126.com>
2017-12-08 12:27:18 +0800

    feature: add notify and html web hints

--- static/js/room.js
+++ static/js/room.js
@@ -13,6 +13,8 @@ var log = console.log;
 var md = navigator.mediaDevices;
 var lv = $("#local_video");
 var rv = $("#remote_video");
+var ls = $("#local_status");
+var rs = $("#remote_status");
 var btn_pub = $("#btn_pub");
 var btn_sub = $("#btn_sub");
 var btn_stop = $("#btn_stop");
@@ -58,20 +60,63 @@ function sendcmd_play(sdp) {
     var msg = { "command": "play", "sdp": sdp };
     ws.send(JSON.stringify(msg));
 }
+function set_pc_notify(pc) {
+    pc.onconnectionstatechange = function(e) {
+        log("notify=> onconnectionstatechange: " + pc.connectionState);
+    };
+    pc.oniceconnectionstatechange = function(e){
+        log("notify=> oniceconnectionstatechange: " + pc.iceConnectionState);
+        rs.html(pc.iceConnectionState);
+    };
+    pc.onicegatheringstatechange = function(e){
+        log("notify=> onicegatheringstatechange: " + pc.iceGatheringState);
+    };
+    pc.onidentityresult = function(e) {
+        log("notify=> onidentityresult: " + e.assertion);
+    };
+    pc.onidpassertionerror = function(e) {
+        log("notify=> onidpassertionerror");
+        log(e);
+    };
+    pc.onidpvalidationerror = function(e) {
+        log("notify=> onidpvalidationerror");
+        log(e);
+    };
+    pc.onnegotiationneeded = function(e) {
+        log("notify=> onnegotiationneeded");
+    };
+    pc.onpeeridentity = function(e) {
+        log("notify=> onpeeridentity");
+        log(e);
+    };
+    pc.onremovestream = function(e) {
+        log("notify=> onremovestream: " + e.stream.id);
+    };
+    pc.onsignalingstatechange = function(e) {
+        log("notify=> onsignalingstatechange: " + pc.signalingState);
+        ls.html(pc.signalingState);
+    };
+}
 
 function do_pub() {
-    log("do pub");
+    ls.html("");
+    rs.html("");
+    log("");
     role = "puber";
     btn_disable(true, true, false);
     candi = [];
+    log("flow=> new RTCPeerConnection");
     pc = new RTCPeerConnection(rtc_cfg);
+    set_pc_notify(pc);
     pc.onicecandidate = function(evt) {
         if (evt.candidate) return;
         if (role === "puber") {
+            log("flow=> ice gathering finished, then send pub command(with offer sdp) to signal server.");
             sendcmd_pub(pc.localDescription);
-        } else if(role === "suber") {
-            sendcmd_play(pc.localDescription);
         }
+    };
+    pc.ontrack = function(e) {
+        console.log("notify=> on track");
     };
     var contraints = { audio: true, video: { width: 640, height: 480 }};
 
@@ -79,39 +124,47 @@ function do_pub() {
         localstream = stream;
         lv.prop("srcObject", stream);
         stream.getTracks().forEach(track => pc.addTrack(track, stream));
+        log("flow=> create offer");
         pc.createOffer({offerToReceiveVideo: false, offerToReceiveAudio: false})
             .then(function(sdp) {
+                log("flow=> offer created, then set local description")
                 pc.setLocalDescription(sdp);
             }).catch(err_handler);
     };
 
+    log("flow=> get user media");
     md.getUserMedia(contraints).then(create_offer).catch(err_handler);
 }
 
 function do_sub() {
-    log("do sub");
     role = "suber";
     btn_disable(true, true, false);
+    ls.html("");
+    rs.html("");
+    log("");
+    log("flow=> new RTCPeerConnection");
     pc = new RTCPeerConnection(rtc_cfg);
+    set_pc_notify(pc);
     pc.onicecandidate = function(evt) {
         if (evt.candidate) return;
         if(role === "suber") {
-            log("send play")
-            log(pc.localDescription.toJSON());
+            log("flow=> ice gathering finished, then send play command(with answer sdp) to signal server.");
             sendcmd_play(pc.localDescription);
         }
     };
+    log("flow=> send subscribe command to server");
     sendcmd_sub();
 }
 
 function do_stop() {
-    log("do stop");
     btn_disable(false, false, false);
     set_stream(null, null);
     if (pc && pc.signalingState != "closed") {
+        log("flow=> close RTCPeerConnection");
         pc.close();
     }
     pc = null;
+    log("flow=> close local or remote stream if needed");
     if (localstream) close_stream(localstream);
     if (remotestream) close_stream(remotestream);
     localstream = null;
@@ -119,27 +172,31 @@ function do_stop() {
 }
 
 ws.onopen = function(e) {
-    log("ws open");
+    log("flow=> ws open");
 }
 ws.onclose = function(e) {
-    log("ws close");
+    log("flow=> ws close");
 }
 ws.onmessage = function(e) {
     var msg = JSON.parse(e.data);
     if (msg.type === "offer") {
+        log("flow=> get offer from signal server, set remote description");
         pc.setRemoteDescription(msg).then(function(){
-            log("set remote sdp");
         }).catch(err_handler);
 
+        log("flow=> create answer");
         pc.createAnswer().then(function(sdp) {
+            log("flow=> answer created, then set local description");
             pc.setLocalDescription(sdp);
         }).catch(err_handler);
 
         pc.ontrack = function(e) {
+            log("notify=> new track: " + e.track.kind);
             set_stream(null, e.streams[0]);
             remotestream = e.streams[0];
         };
     } else if (msg.type === "answer") {
+        log("flow=> get answer from signal server, then set remote description");
         pc.setRemoteDescription(msg);
     }
 }
